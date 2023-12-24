df561901c17e220fa7ed9eb638ad57436b060525
loki192023 <134685561+loki192023@users.noreply.github.com>
2023-12-24 15:35:23 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://t.me/Tamil247Entertainments'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/Tamil247Entertainments'>Click here</a>\n○ Channel : @Tamil247Entertainments\n○ Support Group : @Tamil247Entertainment</b>",
+            text = f"<b>○ Creator : <a href='https://t.me/Tamil247Entertainments'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/Tamil247Entertainments'>Click here</a>\n○ Channel : @Tamil247Entertainment\n○ Support Group : @Tamil247Entertainments</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
